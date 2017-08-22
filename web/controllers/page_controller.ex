edecdfc5b4ec2c02ddc5b80a90bbe0271841bde8
Desmond Naranjo User <dhnaranjo@gmail.com>
2017-08-22 17:04:47 -0500

    Grabbing 10 songs title n url

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,5 +1,7 @@
 defmodule PhoenixPg.PageController do
   require Logger
+  require HTTPoison
+
   use PhoenixPg.Web, :controller
 
   def index(conn, _params) do
@@ -10,6 +12,18 @@ defmodule PhoenixPg.PageController do
   def create(conn, params) do
     artist = params["create"]["artist"]
     Logger.info artist
+    client_key = System.get_env("GENIUS_CLIENT_KEY")
+    response = HTTPoison.get!("https://api.genius.com/search?q=#{URI.encode artist}",
+                             ["Authorization": "Bearer #{client_key}"],
+                             [])
+
+    # IO.puts inspect Poison.decode!(response.body)["response"]["hits"]
+
+    response.body
+    |> Poison.decode!
+    |> Kernel.get_in(["response", "hits"])
+    |> Enum.map(& { &1["result"]["title"], &1["result"]["url"]})
+    |> Apex.ap
     render conn, "index.html"
   end
 end
